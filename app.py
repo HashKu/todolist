3abec886b50b16a0059e12be2cf95a8d17d1acaa
HashKu <brunodaniel.lassis@gmail.com>
2022-11-04 21:09:31 +0000

    Html start

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<html><body style="color: Red">Hello, Web!<body></html>'
+    # templates/home.html
+    return render_template('home.html')
 
 @app.route('/bye')
 def bye():
