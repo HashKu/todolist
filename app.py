9772602027ffa8bfe4f7196c35d9b68697da32a1
HashKu <brunodaniel.lassis@gmail.com>
2022-11-18 20:43:23 +0000

    adicionando tasks

--- app.py
+++ app.py
@@ -2,13 +2,19 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+tasks = [
+    {'name': 'Estudar', 'finished': False},
+    {'name': 'Dormir', 'finished': True}
+]
+
 @app.route('/')
 def home():
     # templates/home.html
-    return render_template('home.html')
+    return render_template('home.html', tasks=tasks)
 
 @app.route('/bye')
 def bye():
     return 'Bye'
 
 app.run(debug=True)
+
